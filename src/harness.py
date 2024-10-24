981725b65bc106c07dee5814ef80720fc3b04b6c
Koushik Anand Pirabu <koushik.prabu@gmail.com>
2024-10-24 19:50:20 +1100

    Basic crash detection implementation

--- src/harness.py
+++ src/harness.py
@@ -6,8 +6,10 @@ class Harness():
     def __init__(self, input_file):
         file_type = from_file(input_file)
         if "CSV" in file_type:
+            print("Switching to CSV mutator")
             self.strategy = "CSV"
         elif "JSON" in file_type:
+            print("Switching to JSON mutator")
             self.strategy = "JSON"
         else:
             print("No matching strategy found, defaulting to plaintext")
