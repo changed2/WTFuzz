654382524b4c0647ff351f5b18f177e5488ce5d2
akanksha <akanksha.sood08@gmail.com>
2024-11-13 15:33:21 +1100

    refactor csv mutations

--- src/strategies/CSV.py
+++ src/strategies/CSV.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import csv
+from mutators.negative_number_replacement import replace_numbers_with_negatives
+from mutators.random_character_append import append_random_characters
+from mutators.row_column_addition import add_rows_and_columns
 import random
 import re
 from collections import UserList
@@ -10,39 +13,6 @@ from fuzzer import random_input
 class CSVObject(UserList):
     def __init__(self, data=None):
         super().__init__(data or [])
-    
-    # MUTATION METHODS
-    def append_characters(self, mutation_count=10):
-        for _ in range(mutation_count):
-            # get a random row and col
-            row = random.randint(1, len(self.data) - 1)
-            col = random.randint(0, len(self.data[row]) - 1)
-            random_str = random_input(100, ord('a'), 26)
-            self.data[row][col] = str(self.data[row][col]) + random_str
-
-    # Strategy 2: replace numbers with negatives
-    def replace_with_negatives(self, mutation_count=10):
-        for _ in range(mutation_count):
-            row = random.randint(1, len(self.data) - 1)
-            col = random.randint(0, len(self.data[row]) - 1)
-            if re.match(r"^[0-9]+(\.[0-9]+)?$", self.data[row][col]):
-                self.data[row][col] = "-" + self.data[row][col]
-
-    # Strategy 3: Add more rows and columns
-    def add_rows_and_cols(self, mutation_count=10):
-        # For each mutation, randomly add anywhere between 10-100 rows/cols.
-        for _ in range(mutation_count):
-            new_num_rows = random.randint(10, 100)
-            new_num_cols = random.randint(10, 100)
-
-            for _ in range(new_num_rows):
-                new_row = [random.choice(self.data[random.randint(0, len(self.data) - 1)]) for _ in range(len(self.data[0]))]
-                self.append(new_row)
-                
-            for _ in range(new_num_cols):
-                new_values = [random.choice(self.data[random.randint(0, len(self.data) - 1)]) for _ in range(len(self.data))]
-                for i in range(len(self.data)):
-                    self.data[i].append(new_values[i])
 
 # Read csv file contents
 def read_csv(input_file):
@@ -66,11 +36,11 @@ def list_to_csv(csv_object):
 def mutate_csv(csv_input_file, binary_file, harness):
     csv_object = read_csv(csv_input_file)
 
-    csv_mutator = [csv_object.append_characters, csv_object.replace_with_negatives, csv_object.add_rows_and_cols]
+    csv_mutator = [append_random_characters, replace_numbers_with_negatives, add_rows_and_columns]
 
     # Apply each mutation method to the csv_object
     for mutator in csv_mutator:
-        mutator() 
+        mutator(csv_object) 
         fuzzed_data = list_to_csv(csv_object)
         harness.run_retrieve(binary_file, fuzzed_data)
 
