9435fd58ef0a0b0c291500c9076b5fdf84553309
akanksha <akanksha.sood08@gmail.com>
2024-11-13 16:52:38 +1100

    Update mutation methods

--- src/mutators/random_character_append.py
+++ src/mutators/random_character_append.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-
 import random
-
-def append_random_characters(data, count=10, max_length=100):
-    # Append random characters to various fields in the CSV data.
-    for _ in range(count):
-        row = random.randint(1, len(data) - 1)
-        col = random.randint(0, len(data[row]) - 1)
-        random_str = ''.join(chr(random.randint(ord('a'), ord('z'))) for _ in range(random.randint(1, max_length)))
-        data[row][col] += random_str
+from fuzzer import random_input
+def append_random_characters(data, mutation_count=10):
+        for _ in range(mutation_count):
+            # get a random row and col
+            row = random.randint(1, len(data) - 1)
+            col = random.randint(0, len(data[row]) - 1)
+            random_str = random_input(100, ord('a'), 26)
+            data[row][col] = str(data[row][col]) + random_str
