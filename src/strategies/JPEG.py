e5279d88dffb2485fbbb9eb7cdae9dafbd29a91c
akanksha <akanksha.sood08@gmail.com>
2024-11-15 02:37:59 +1100

    Add 2 mutators

--- src/strategies/JPEG.py
+++ src/strategies/JPEG.py
@@ -5,9 +5,9 @@
 
 import os
 import base64
-from mutators.byteflip import byte_flip
-from mutators.bitflip import bit_flip
-
+from mutators.random_byte_flip import flip_random_bytes
+from mutators.remove_segment import remove_random_segment
+from mutators.dqt_manipulation import corrupt_dqt_table
 def read_jpeg(file_path):
     with open(file_path, 'rb') as file:
         return file.read()
@@ -18,13 +18,10 @@ def write_jpeg(data, file_path):
         file.write(data)
 
 def mutate_jpeg(input_file, binary, harness):
-    """Applies a series of mutations to a JPEG, then runs the mutated data through a harness."""
     original_data = read_jpeg(input_file)
 
     mutated_data = original_data
-    for mutation in [byte_flip, bit_flip]:
-        mutated_data = mutation(mutated_data)  # Apply mutation and update mutated_data
-
-    base64_encoded_data = base64.b64encode(mutated_data).decode('utf-8')
-
-    harness.run_retrieve(binary, base64_encoded_data)
+    for mutation in [flip_random_bytes, remove_random_segment, corrupt_dqt_table]:
+        mutated_data = mutation(mutated_data)
+        base64_encoded_data = base64.b64encode(mutated_data).decode('utf-8')
+        harness.run_retrieve(binary, base64_encoded_data)
