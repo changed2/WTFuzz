01d7659e34956cb9759aaeea8ee09cec76b0e987
akanksha <akanksha.sood08@gmail.com>
2024-11-15 02:14:53 +1100

    add bit_flip mutation for jpg

--- src/strategies/JPEG.py
+++ src/strategies/JPEG.py
@@ -6,6 +6,7 @@
 import os
 import base64
 from mutators.byteflip import byte_flip
+from mutators.bitflip import bit_flip
 
 def read_jpeg(file_path):
     with open(file_path, 'rb') as file:
@@ -16,11 +17,14 @@ def write_jpeg(data, file_path):
     with open(file_path, 'wb') as file:
         file.write(data)
 
-
 def mutate_jpeg(input_file, binary, harness):
+    """Applies a series of mutations to a JPEG, then runs the mutated data through a harness."""
     original_data = read_jpeg(input_file)
 
-    mutated_data = byte_flip(original_data)
+    mutated_data = original_data
+    for mutation in [byte_flip, bit_flip]:
+        mutated_data = mutation(mutated_data)  # Apply mutation and update mutated_data
+
     base64_encoded_data = base64.b64encode(mutated_data).decode('utf-8')
 
     harness.run_retrieve(binary, base64_encoded_data)
