1d9a8363ce7cae1c32abcaf35255567293b82231
akanksha <akanksha.sood08@gmail.com>
2024-11-15 02:03:44 +1100

    Try crash jpg

--- src/strategies/JPEG.py
+++ src/strategies/JPEG.py
@@ -1,57 +1,26 @@
 # #!/usr/bin/env python3
 
-# import random
-# from collections import UserList
+# https://dev.exiv2.org/projects/exiv2/wiki/The_Metadata_in_JPEG_files
+# https://docs.fileformat.com/image/jpeg/ 
 
-# class JPEGMutator(UserList):
-#     def __init__(self, data):
-#         super().__init__(data)
+import os
+import base64
+from mutators.byteflip import byte_flip
 
-#     def modify_magic_numbers(self):
-#         magic_values = [
-#             (1, [0xff, 0x7f, 0x00]),
-#             (2, [0xffff, 0x0000]),
-#             (4, [0xffffffff, 0x00000000, 0x80000000, 0x40000000, 0x7fffffff])
-#         ]
-#         length = len(self.data) - 8
-#         for size, values in magic_values:
-#             idx = random.randint(0, length)
-#             value = random.choice(values)
-#             for i in range(size):
-#                 self.data[idx + i] = (value >> (8 * (size - 1 - i))) & 0xff
+def read_jpeg(file_path):
+    with open(file_path, 'rb') as file:
+        return file.read()
 
-#     def tweak_bits(self, count=100):
-#         length = len(self.data) - 4
-#         for _ in range(count):
-#             idx = random.randint(2, length)
-#             bit = 1 << random.randint(0, 7)
-#             self.data[idx] ^= bit
-
-# def read_jpeg_text(input_file):
-#     with open(input_file, 'r') as file:
-#         hex_data = file.read().strip()
-#     jpeg_data = bytes.fromhex(hex_data)
-#     return JPEGMutator(jpeg_data)
-
-# def save_jpeg(jpeg_mutator, output_file):
-#     with open(output_file, 'wb') as file:
-#         file.write(bytes(jpeg_mutator))
-
-# def mutate_jpeg(input_file, output_file, harness):
-#     jpeg_mutator = read_jpeg_text(input_file)
-#     jpeg_mutator.modify_magic_numbers()
-#     jpeg_mutator.tweak_bits()
-
-#     save_jpeg(jpeg_mutator, output_file)
-
-#     mutated_data = bytes(jpeg_mutator)
-#     harness.run_retrieve(output_file, mutated_data)
+def write_jpeg(data, file_path):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, 'wb') as file:
+        file.write(data)
 
 
+def mutate_jpeg(input_file, binary, harness):
+    original_data = read_jpeg(input_file)
 
-# # A JPEG image consists of a sequence of segments,
-# # each beginning with a marker, each of which begins
-# # with a 0xFF byte, followed by a byte indicating what
-# # kind of marker it is.
+    mutated_data = byte_flip(original_data)
+    base64_encoded_data = base64.b64encode(mutated_data).decode('utf-8')
 
-
+    harness.run_retrieve(binary, base64_encoded_data)
